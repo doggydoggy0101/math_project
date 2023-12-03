a070143cc26d31649469296fcbce3d4d36a56a15
Bang-Shien Chen <dgbshien@gmail.com>
2023-12-03 22:01:24 +0800

    Add files via upload

--- regression_analysis/group_project/dataTrainTest.py
+++ regression_analysis/group_project/dataTrainTest.py
@@ -13,3 +13,14 @@ print("num of test:", test.shape[0])
 
 train.to_csv("data/math_score_train.csv", encoding='utf-8', index=False)
 test.to_csv("data/math_score_test.csv", encoding='utf-8', index=False)
+
+
+data2 = pd.read_csv('data/math_score_interact.csv')
+
+train2, test2 = train_test_split(data2, test_size=0.2, random_state=rng)
+
+# print("num of train:", train.shape[0])
+# print("num of test:", test.shape[0])
+
+train2.to_csv("data/math_score_train2.csv", encoding='utf-8', index=False)
+test2.to_csv("data/math_score_test2.csv", encoding='utf-8', index=False)
