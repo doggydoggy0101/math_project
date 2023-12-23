87e19787a6b395db725f6863f049c1a8551d52b9
Bang-Shien Chen <dgbshien@gmail.com>
2023-12-23 15:28:53 +0800

    Add files via upload

--- linear_programming/run.py
+++ linear_programming/run.py
@@ -1,7 +1,7 @@
 import numpy as np 
 from fractions import Fraction
 np.set_printoptions(formatter={'all':lambda x: str(Fraction(x).limit_denominator())})
-from simplex import simplex_iteration
+from simplex import Simplex
 
 # Ex 
 #     -6x1     +x3 -2x4 +2x5 =6
@@ -13,49 +13,15 @@ X = np.array([[ -6,  0,  1, -2,  2,  6],
               [  5,  0,  0,  3, -2,-21]], dtype=float)
 
 # Ex 
-#     -2x1      +6x3 +2x4      -3x6 + x7 =20
-#     -4x1 + x2 +7x3 + x4      - x6      =10
-#     -5x3           +3x4 + x5 - x6      =60
-#     13x3           -6x4      +2x6      =110
+#      3x1 + 2x2       + x4           =60
+#      -x1 +  x2 + 4x3      + x5      =10
+#      2x1 - 2x2 + 5x3           + x6 =50
+#     -2x1 - 3x2 - 3x3                = 0
 
-X = np.array([[ -2,  0,  6,  2,  0, -3,  1, 20],
-              [ -4,  1,  7,  1,  0, -1,  0, 10],
-              [  0,  0, -5,  3,  1, -1,  0, 60],
-              [  0,  0, 13, -6,  0,  2,  0,110]], dtype=float)
+X = np.array([[  3,  2,  0,  1,  0,  0, 60],
+              [ -1,  1,  4,  0,  1,  0, 10],
+              [  2, -2,  5,  0,  0,  1, 50],
+              [ -2, -3, -3,  0,  0,  0,  0]], dtype=float)
 
-
-simplex_iteration(X)
-
-
-# linear equation system:
-# -2.0x1 +6.0x3 +2.0x4 -3.0x6 +1.0x7 =20.0
-# -4.0x1 +1.0x2 +7.0x3 +1.0x4 -1.0x6 =10.0
-# -5.0x3 +3.0x4 +1.0x5 -1.0x6 =60.0
-# 13.0x3 -6.0x4 +2.0x6 =110.0
-
-# --------------------------------------------------
-
-# 1 iteration:
-# [[6 -2 -8 0 0 -1 1 0]
-#  [-4 1 7 1 0 -1 0 10]
-#  [12 -3 -26 0 1 2 0 30]
-#  [-24 6 55 0 0 -4 0 170]]
-
-# 2 iteration:
-# [[1 -1/3 -4/3 0 0 -1/6 1/6 0]
-#  [0 -1/3 5/3 1 0 -5/3 2/3 10]
-#  [0 1 -10 0 1 4 -2 30]
-#  [0 -2 23 0 0 -8 4 170]]
-
-# 3 iteration:
-# [[1 -7/24 -7/4 0 1/24 0 1/12 5/4]
-#  [0 1/12 -5/2 1 5/12 0 -1/6 45/2]
-#  [0 1/4 -5/2 0 1/4 1 -1/2 15/2]
-#  [0 0 3 0 2 0 0 230]]
-
-# --------------------------------------------------
-
-# iterations: 3
-# basic variables (index): [1 4 6]
-# basic feasible solution: [5/4 0 0 45/2 0 15/2 0]
-# minimize: -230.0
+sol = Simplex(CanonicalForm=X, verbose=True)
+sol.iterate()
--- linear_programming/simplex.py
+++ linear_programming/simplex.py
@@ -1,101 +1,117 @@
 import numpy as np 
 
-def simplex_iteration(X):
+class Simplex:
+    def __init__(self, CanonicalForm, verbose=True):
 
-    m = X.shape[0] - 1
-    n = X.shape[1] - 1
+        self.X = CanonicalForm
+        self.verbose = verbose
 
-    print("linear equation system:")
-    for row in range(m+1):
-        text = ""
-        for i in range(n):
-            if X[row, i] != 0:
-                text_i = "+" + str(X[row, i]) + "x" + str(i+1) + " " if text != "" and X[row, i] > 0 else str(X[row, i]) + "x" + str(i+1) + " "
-                text += text_i
-        text_i = "=" + str(X[row, n])
-        text += text_i
-        print(text)
-    print("\n" + "-"*50)
+        self.m = self.X.shape[0] - 1
+        self.n = self.X.shape[1] - 1
 
-    F = np.zeros(n) # 給 basic feasible sol空間
-    label = np.zeros(m) # 給 table左邊那排 label空間
-    tt = 0 # 係數為正的數量
-    num = 0 # 跌代次數
-    while tt != n:
+        if self.verbose: self.dispEquation()
 
-        tlabel=0
-        tt=0 # 係數為正的數量
+    def dispEquation(self):
 
-        # 找basic variable當label
-        for j in np.arange(m):
-            for i in np.arange(n):
-                if X[j][i]==1 and np.sum(X[:,i])==1:
-                    label[j]=i+1
+        print("\n" + "-"*13 + " Linear Equation System " + "-"*14)
+        for row in range(self.m + 1):
+            text = ""
+            for i in range(self.n):
+                if self.X[row, i] != 0:
+                    if text != "" and self.X[row, i] > 0:
+                        text_i = "+" + "x" + str(i+1) + " " if self.X[row, i] == 1 else "+" + str(self.X[row, i]) + "*x" + str(i+1) + " "
+                    else:
+                        text_i = "x" + str(i+1) + " " if self.X[row, i] == 1 else str(self.X[row, i]) + "*x" + str(i+1) + " "
+                    text += text_i
+            text_i = "=" + str(self.X[row, self.n])
+            text += text_i
+            print(text)
+        # print("\n" + "-"*50 + "\n")
 
-        mlabel=np.array([]) # 係數為負
-        for t in np.arange(n):
-            if X[m,t] >= 0:
-                tt=tt+1
-            else:
-                mlabel=np.append(mlabel, t)
-
-        if tt ==n: # 全部係數都為正 print答案
-            sol=X[m,n]
-            
-            for f in np.arange(m):
-                ff=int(label[f]-1)
-                F[ff]=X[f,n]
-            
-            print("\n" + "-"*50 +"\n")
-            print("iterations:",str(num))
-            print("basic variables (index):", str(label))
-            print("basic feasible solution:", str(F))
-            print("minimize:", str(-sol))
-            break
-
-        # 找目標含數裡係數負的 min 得出pivoting column
-        M=np.array([])
-        for mm in np.arange(len(mlabel)):
-            M=np.append(M,X[m,int(mlabel[mm])])
-
-        M=np.min(M)
-
-        for mmm in np.arange(n):
-            if X[m,mmm]==M:
-                ttt=mmm 
+    def dispSol(self):
         
-        tlabel=ttt # column
+        print("\n" + "-"*21 + " Summary " + "-"*21)
+        print("iterations:",str(self.iters))
+        print("basic variables (index):", str(self.label))
+        print("basic feasible solution:", str(self.sol_set))
+        print("optimal:", str(-self.sol))
 
-        # 找column上面係數為正
-        Bb=np.array([])
-        for k in np.arange(m):
-            if X[k,tlabel]>0:
-                Bb=np.append(Bb,k)
+    def dispIter(self):
 
-        # 找column上面係數為正 且 b/a的 min
-        B=np.array([])
+        print("initial system:") if self.iters == 0 else print("\n{} iteration:".format(self.iters))
+        print(self.X) 
 
-        for kk in np.arange(len(Bb)):
-            bbb=int(Bb[kk])
-            B=np.append(B,X[bbb,n]/X[bbb,tlabel])
 
-        B=np.min(B)   
-        for kkk in np.arange(m):   
-            if X[kkk,n]/X[kkk,tlabel]==B:
-                ll=kkk # ll是pivot row
+    def iterate(self): 
+        if self.verbose: print("\n" + "-"*20 + " Iteration " + "-"*20)
 
-        label[ll]=tlabel+1 # tlabel是column
+        self.label = np.zeros(self.m) # basic variable label
+        self.iters = 0 # num of iterations
+        basic_var = 0 # num of basic varialbes
 
-        # pivot係數變1
-        if X[ll,tlabel] !=1:
-            X[ll,:]=X[ll,:]/X[ll,tlabel]
+        while basic_var != self.n:
 
-        # pivot其他變0
-        Xtemp = X[ll,:].copy()
-        for kkkk in np.arange(m+1):
-            X[kkkk,:]=X[kkkk,:]-X[kkkk,tlabel]*Xtemp
-        X[ll,:]=Xtemp
+            if self.verbose: self.dispIter()
 
-        print("\n{} iteration:".format(num+1))
-        print(X) 
-        num += 1
+            # set pivot points as basic variable 
+            for i in range(self.m):
+                for j in range(self.n):
+                    if self.X[i][j] == 1 and np.sum(self.X[:,j]) == 1:
+                        self.label[i] = j+1
+
+            # compute positive and negative coefficient
+            basic_var = 0 # num of nonnegative basic variables
+            neg_label = [] # negative coefficient
+            for j in range(self.n):
+                if self.X[self.m, j] >= 0:
+                    basic_var += 1
+                else:
+                    neg_label.append(j)
+
+            # all labels are nonnegative, return solution
+            if basic_var == self.n:
+
+                self.sol = self.X[self.m, self.n] 
+                self.sol_set = np.zeros(self.n) # basic feasible sol
+
+                for i in range(self.m):
+                    basic_var_idx = int(self.label[i]-1)
+                    self.sol_set[basic_var_idx] = self.X[i, self.n]
+                break
+
+            ### simplex iteration
+
+            # choose the smallest nonnegative coefficient
+            neg_coef_min = 0
+            for neg_idx in range(len(neg_label)):
+                neg_coef = self.X[self.m,int(neg_label[neg_idx])]
+                if neg_coef < neg_coef_min:
+                    neg_coef_min = neg_coef
+
+            # get pivoting variable column
+            pivot_col = np.where(self.X[self.m,:] == neg_coef_min)[0][0]
+
+            # get pivoting variable row
+            ratio_min = 1e+7
+            for i in range(self.m):
+                if self.X[i, pivot_col] > 0:
+                    ratio = self.X[i, self.n] / self.X[i, pivot_col]
+                    if ratio < ratio_min:
+                        ratio_min = ratio
+                        pivot_row = i
+
+            # update basic variable label
+            self.label[pivot_row] = pivot_col + 1
+
+            # pivoting
+            if self.X[pivot_row, pivot_col] != 1:
+                self.X[pivot_row, :] = self.X[pivot_row, :] / self.X[pivot_row, pivot_col]
+
+            temp = self.X[pivot_row, :].copy()
+            for i in range(self.m+1):
+                self.X[i,:] -= self.X[i, pivot_col]*temp
+            self.X[pivot_row, :] = temp
+
+            self.iters+=1
+
+        if self.verbose: self.dispSol()
