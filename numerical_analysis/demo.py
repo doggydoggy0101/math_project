89fc2d4d6539023ba87eb6adc97c3b71ed8f5716
Bang-Shien <dgbshien@gmail.com>
2024-07-24 23:53:21 -0500

    add numercial analysis

--- numerical_analysis/demo.py
+++ numerical_analysis/demo.py
@@ -1,4 +1,6 @@
-from rootfind import newton_method
+from rootfind import newton_method, bisection_method, fix_point_method
+from sympy import Symbol 
+sym_x = Symbol("x")
 verbose = True
 
 def f(x): 
@@ -7,6 +9,24 @@ def f(x):
 def df(x): 
     return 2*x
 
+print("\nroot finding function:", f(sym_x))
+
+print("="*3 + " solved by Newton's method " + "="*3)
 model = newton_method(verbose=verbose)
 model.solve(f, df, initial_point=1)
+
+print("="*3 + " solved by bisection method " + "="*3)
+model = bisection_method(verbose=verbose)
+model.solve(f, initial_bound=[1, 4])
+
+
+
+def g(x): 
+    return (3*x**2 + 3)**(1/4)
+
+print("\nfix point finding function:", g(sym_x))
+
+print("="*3 + " solved by fix point method " + "="*3)
+model = fix_point_method(verbose=verbose)
+model.solve(g, initial_point=1)
         
--- numerical_analysis/rootfind.py
+++ numerical_analysis/rootfind.py
@@ -21,6 +21,66 @@ class newton_method:
                 break
 
         if self.verbose:
-            print("solved by Newton's method:", x)
+            print("solution:", x)
         return x
 
+
+
+class bisection_method:
+
+    def __init__(self, max_iterations=1000, verbose=False):
+       
+        self.max_iter = max_iterations
+        self.verbose = verbose
+
+    def solve(self, objective, initial_bound):
+
+        a = initial_bound[0]
+        b = initial_bound[1]
+
+        for i in range(self.max_iter):
+
+            x = (a + b)/2
+
+            if np.linalg.norm(objective(x)) < 1e-10:
+                if self.verbose:
+                    print("iterations:", i+1)
+                break
+            elif objective(x)*objective(a) < 0:
+                b = x
+            else:
+                a = x
+
+        if self.verbose:
+            print("solution:", x)
+        return x
+
+
+
+class fix_point_method:
+
+    def __init__(self, max_iterations=1000, tolerance=1e-7, verbose=False):
+       
+        self.max_iter = max_iterations
+        self.tol = tolerance
+        self.verbose = verbose
+
+    def solve(self, objective, initial_point):
+
+        x = initial_point
+
+        for i in range(self.max_iter):
+
+            x_k = objective(x)
+
+            step_size = np.linalg.norm(x - x_k)
+            if step_size < self.tol:
+                if self.verbose:
+                    print("iterations:", i+1)
+                break
+
+            x = x_k
+
+        if self.verbose:
+            print("solution:", x)
+        return x
