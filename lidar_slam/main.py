6a286017efa5069888e76b296be0e1f7460e9361
Bang-Shien <dgbshien@gmail.com>
2024-09-18 11:25:22 +0800

    :dog: update results

--- lidar_slam/main.py
+++ lidar_slam/main.py
@@ -2,10 +2,10 @@ from utils.dataloader import LoadKITTIdataset
 from model.loam import LaserOdometryAndMapping
 from utils.evaluate import EvalOdometry
 
-SEQUENCE="04"
+SEQUENCE="00"
 
 data = LoadKITTIdataset(data_path="data", sequence=SEQUENCE)
-model = LaserOdometryAndMapping()
+model = LaserOdometryAndMapping(voxel_size=1)
 model.run(data)
 
 EvalOdometry().run(sequence=SEQUENCE, alignment="6dof")
